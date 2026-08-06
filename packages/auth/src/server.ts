c6d2a99a72d85f9ad5c8a0038589e4e0edea831e
urpirio <urpiriojunior@gmail.com>
2026-08-06 11:14:48 -0400

    chore: update packages/auth/src/server.ts

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -1,1 +1,24 @@
-export { initAuth } from "./auth";
+export {
+  signUpWithPassword,
+  signInWithPassword,
+  resetPasswordForEmail,
+  signOut,
+  getSession,
+  deleteUser,
+  setUserPassword,
+} from "./auth";
+
+export { getSupportedOAuthProviders } from "./providers";
+
+export {
+  isSignUpAllowed,
+  checkSignUpAllowed,
+  handlePostSignupAvatar,
+} from "./hooks";
+
+export {
+  parseInviteCallbackParams,
+  completeInvite,
+  sendMagicLink,
+  sendInviteMagicLink,
+} from "./magic-link";
